d43bbd3819d84ad2b6fc433f317735a48c53d018
Arsene Lupin III <70369063+jimenezjl@users.noreply.github.com>
2023-04-07 13:09:16 +0200

    Update 100m.py
    Tomando como referencia el código de los 400 metros lisos. Programa listo para que una persona pueda jugar.
    Faltan mensajes de error cuando no se terminan los dos ciclos, o cuando en el séptimo lanzamiento sale un 6.

--- 100m.py
+++ 100m.py
@@ -3,45 +3,44 @@ import random
 def diceroll():
     return random.randint(1, 6)
 
-# Primer ciclo de lanzamientos
+global num_lanzamientos, dados_guardados, pares_guardados, suma_total
 num_lanzamientos = 1
-lanzamiento_valido = False
-while num_lanzamientos <= 7 and not lanzamiento_valido:
-    dados = [diceroll() for _ in range(4)]
-    suma_dados = sum(dados)
-    print(f"Resultado del lanzamiento {num_lanzamientos}: {dados}. Suma total: {suma_dados}")
+dados_guardados = []
+pares_guardados = 1
+suma_total = 0
 
-    if 6 not in dados:
-        guardar_resultado = input("¿Quieres guardar este resultado? (s/n): ")
-        if guardar_resultado.lower() == 's':
-            cincuentam = suma_dados
-            lanzamiento_valido = True
-        else:
-            num_lanzamientos += 1
-    else:
-        print(f"Al menos uno de los dados ha sido un 6, debes volver a lanzar los dados. Recuerda que tienes un máximo de 7 lanzamientos y este es tu lanzamiento número {num_lanzamientos}")
-        num_lanzamientos += 1
-
-# Segundo ciclo de lanzamientos sin reiniciar el contador de lanzamientos
-if lanzamiento_valido:
-    suma_total = cincuentam
-    while num_lanzamientos <= 7:
+def lanzar_dados():
+    global num_lanzamientos, dados_guardados, pares_guardados, suma_total
+    lanzamiento_valido = False
+    while num_lanzamientos <= 7 and pares_guardados < 3:
+        print(f"Este es tu lanzamiento número {num_lanzamientos} y estás en tu ciclo de lanzamientos número {pares_guardados} de 2.")
+        input("Pulsa enter para lanzar los dados.")
         dados = [diceroll() for _ in range(4)]
         suma_dados = sum(dados)
-        print(f"Resultado del lanzamiento {num_lanzamientos}: {dados}. Suma total: {suma_dados + suma_total}")
+        print(f"Resultado del lanzamiento {num_lanzamientos}: {dados}. Suma del lanzamiento: {suma_dados} Suma total: {suma_total+suma_dados}")
 
         if 6 not in dados:
             guardar_resultado = input("¿Quieres guardar este resultado? (s/n): ")
             if guardar_resultado.lower() == 's':
+                dados_guardados.append(dados)
+                pares_guardados += 1
+                num_lanzamientos += 1
                 suma_total += suma_dados
-                break
+                lanzamiento_valido = True
             else:
                 num_lanzamientos += 1
         else:
-            if num_lanzamientos == 7:
-                print("Error: se ha alcanzado el máximo de lanzamientos y se ha obtenido un 6")
-                break
-            print(f"Al menos uno de los dados ha sido un 6, debes volver a lanzar los dados. Recuerda que tienes un máximo de 7 lanzamientos y este es tu lanzamiento número {num_lanzamientos}")
+            print(f"Al menos uno de los dados ha sido un 6, debes volver a lanzar los dados. Recuerda que tienes un máximo de 9 lanzamientos y este es tu lanzamiento número {num_lanzamientos}")
             num_lanzamientos += 1
-
-    print(f"Suma total: {suma_total}")
+    if lanzamiento_valido:
+        return dados_guardados
+    else:
+        return None, num_lanzamientos
+    
+resultados = lanzar_dados()
+if resultados:
+    print(f"Los resultados válidos son: {resultados}")
+else:
+    print("No se ha guardado ningún resultado válido.")
+print(f"La cantidad de pares de dados guardados es: {pares_guardados}")
+print(f"El valor final de tu carrera de 100 metros lisos es: {suma_total}")
