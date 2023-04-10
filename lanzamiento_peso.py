8313ea33cbf8862739b4ba1da2426d0fbc636948
Arsene Lupin III <70369063+jimenezjl@users.noreply.github.com>
2023-04-10 12:43:25 +0200

    Actualización de Lanzamiento de Peso
    Se han añadido 3 intentos. Al finalizar te muestra los 3 lanzamientos incluidos los resultados nulos.

--- lanzamiento_peso.py
+++ lanzamiento_peso.py
@@ -12,10 +12,12 @@ max_shotput = 0
 def attempt():
     global max_shotput
     total_shotput = []
+    results = []
     for i in range(3):
         total_score = 0
         num_of_dice = 0
         print(f"INTENTO {i+1}/3")
+        input("Pulsa enter para lanzar")
         while num_of_dice < 8:
             num_of_dice += 1
             dice_roll = random.randint(1, 6)
@@ -27,6 +29,7 @@ def attempt():
                 break
             if num_of_dice == 8:
                 total_shotput.append(total_score)
+                results.append([i+1, total_score])
                 print(f"Lanzamiento {i+1} terminado, puntuación total: {total_score}")
                 if total_score > max_shotput:
                     max_shotput = total_score
@@ -34,15 +37,22 @@ def attempt():
             response = input("Si quieres continuar lanzando, pulsa s, si quieres parar el lanzamiento pulsa n, (s/n): ")
             if response.lower() != 's':
                 total_shotput.append(total_score)
+                results.append([i+1, total_score])
                 print(f"Lanzamiento {i+1} terminado, puntuación total: {total_score}")
                 if total_score > max_shotput:
                     max_shotput = total_score
                 break
+        if total_score == 0:
+            results.append([i+1, "Lanzamiento nulo"])
     print("Resultados:")
-    for i, score in enumerate(total_shotput):
-        if score == max_shotput:
-            print(f"Lanzamiento {i+1}: \033[1m{score}\033[0m (máximo)")
+    for i, row in enumerate(results):
+        if isinstance(row[1], int):
+            if row[1] == max_shotput:
+                print(f"Lanzamiento {row[0]}: \033[1m{row[1]}\033[0m (máximo)")
+            else:
+                print(f"Lanzamiento {row[0]}: {row[1]}")
         else:
-            print(f"Lanzamiento {i+1}: {score}")
-            
+            print(f"Lanzamiento {row[0]}: {row[1]}")
+    return results
+
 attempt()
