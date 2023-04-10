430937f842d18aa7ecb02557a5bc031a2914746d
Arsene Lupin III <70369063+jimenezjl@users.noreply.github.com>
2023-04-10 12:10:33 +0200

    Update lanzamiento_peso.py
    Añadida la opción de que haya 3 intentos y la selección del mejor lanzamiento.

--- lanzamiento_peso.py
+++ lanzamiento_peso.py
@@ -7,42 +7,42 @@ Created on Mon Mar 27 09:28:24 2023
 """
 import random
 
-total_score = 0
-total_shotput = []
 max_shotput = 0
 
-def roll_dice():
-    global total_score
-    num_of_dice = 0
-    while num_of_dice < 8:
-        num_of_dice += 1
-        dice_roll = random.randint(1, 6)
-        total_score += dice_roll
-        print(f"Dado {num_of_dice}: {dice_roll}, Suma: {total_score}")
-        if dice_roll == 1:
-            print(f"¡Has sacado un 1 en el dado {num_of_dice}! Tu lanzamiento se considera nulo.")
-            return 0
-        if num_of_dice == 8:
-            return total_score
-        response = input("¿Deseas lanzar otro dado? (s/n): ")
-        if response.lower() != 's':
-            return total_score
-
-def attemp():
-    total_attemp = 0
-    global total_shotput
-    while total_attemp < 3:
-        total_attemp += 1
-        print(f"Estás en el intento {total_attemp} de 3.")
-        total_score = roll_dice()
-        total_shotput.append(total_score)
-        return total_shotput
-
-    print(f"Los resultados de tus 3 intentos son: {total_shotput}")
- 
-
-attemp()
-
-max_shotput = max(total_shotput)
-
-print (f"Tu lanzamiento más largo ha sido {max_shotput}")
+def attempt():
+    global max_shotput
+    total_shotput = []
+    for i in range(3):
+        total_score = 0
+        num_of_dice = 0
+        print(f"INTENTO {i+1}/3")
+        while num_of_dice < 8:
+            num_of_dice += 1
+            dice_roll = random.randint(1, 6)
+            total_score += dice_roll
+            print(f"Dado {num_of_dice}: {dice_roll}, Suma: {total_score}")
+            if dice_roll == 1:
+                print(f"¡Has sacado un 1 en el dado {num_of_dice}! Tu lanzamiento se considera nulo.")
+                total_score = 0
+                break
+            if num_of_dice == 8:
+                total_shotput.append(total_score)
+                print(f"Lanzamiento {i+1} terminado, puntuación total: {total_score}")
+                if total_score > max_shotput:
+                    max_shotput = total_score
+                break
+            response = input("Si quieres continuar lanzando, pulsa s, si quieres parar el lanzamiento pulsa n, (s/n): ")
+            if response.lower() != 's':
+                total_shotput.append(total_score)
+                print(f"Lanzamiento {i+1} terminado, puntuación total: {total_score}")
+                if total_score > max_shotput:
+                    max_shotput = total_score
+                break
+    print("Resultados:")
+    for i, score in enumerate(total_shotput):
+        if score == max_shotput:
+            print(f"Lanzamiento {i+1}: \033[1m{score}\033[0m (máximo)")
+        else:
+            print(f"Lanzamiento {i+1}: {score}")
+            
+attempt()
